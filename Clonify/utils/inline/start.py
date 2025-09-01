4cfc6ce8678ec9fcfe553f56df22f2b3d68cdd98
mad-g0d <madgod3302@gmail.com>
2025-09-01 12:37:45 +0530

    update

--- Clonify/utils/inline/start.py
+++ Clonify/utils/inline/start.py
@@ -29,12 +29,12 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)
+            InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/DUNEBOTS"),
+            InlineKeyboardButton(text=_["S_B_2"], url=f"https://t.me/DUNEBOTS")
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.SOURCE)
+            InlineKeyboardButton(text=_["S_B_7"], url=f"http://t.me/+4cR56ah9h1g5NzA0")
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
